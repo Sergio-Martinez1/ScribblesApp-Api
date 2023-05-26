af7304d1b048b4c88adb3c2e4eda897734c3d615
SergioD <sergio_david2109@hotmail.com>
2023-05-25 20:34:34 -0600

    feat(db, router): set database config and import the user router

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from routes.user import users_route
+from db_config.database import Base, engine
 
 app = FastAPI()
 
-
 app.title = "Scribbles API"
 app.version = "0.0.1"
+app.include_router(users_route)
+
+Base.metadata.create_all(bind=engine)
