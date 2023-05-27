43ecfdf7de9bb83a19e227901f88cd8e30db39f6
SergioD <sergio_david2109@hotmail.com>
2023-05-26 21:19:06 -0600

    feat(db_models): add a relationship field to posts

--- models/reaction.py
+++ models/reaction.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer
 from sqlalchemy.schema import ForeignKey
+from sqlalchemy.orm import relationship
 from db_config.database import Base
 
 
@@ -9,3 +10,4 @@ class Reaction(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     post_id = Column(Integer, ForeignKey('posts.id'))
+    post = relationship('Post', back_populates='reactions')
