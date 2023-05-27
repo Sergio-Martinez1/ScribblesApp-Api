702098d1a8f9b985c964d7bf596732133c78b471
mutska <mutska@ciencias.unam.mx>
2023-05-26 20:38:28 -0600

    feat(db_models): add a new model for comments

---
+++ models/comment.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy.orm import relationship
+from db_config.database import Base
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post = relationship('Post', back_populates='comments')
