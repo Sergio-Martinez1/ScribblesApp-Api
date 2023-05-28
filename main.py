b3caa605b50052281f9479911a03e2e899cc3152
SergioD <sergio_david2109@hotmail.com>
2023-05-27 21:51:05 -0600

    feat(router): include the reactions and tags routers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routes.user import users_route
+from routes.reaction import reactions_router
+from routes.tag import tags_router
 from db_config.database import Base, engine
 
 app = FastAPI()
@@ -7,5 +9,7 @@ app = FastAPI()
 app.title = "Scribbles API"
 app.version = "0.0.1"
 app.include_router(users_route)
+app.include_router(reactions_router)
+app.include_router(tags_router)
 
 Base.metadata.create_all(bind=engine)
