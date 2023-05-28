4405479fd69da6608c179877d2bef5228bc1f0bc
mutska <mutska@ciencias.unam.mx>
2023-05-27 21:59:24 -0600

    chores(update): include comments router in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.user import users_route
 from routes.reaction import reactions_router
+from routes.comment import comments_router
 from routes.tag import tags_router
 from db_config.database import Base, engine
 
@@ -11,5 +12,6 @@ app.version = "0.0.1"
 app.include_router(users_route)
 app.include_router(reactions_router)
 app.include_router(tags_router)
+app.include_router(comments_router)
 
 Base.metadata.create_all(bind=engine)
