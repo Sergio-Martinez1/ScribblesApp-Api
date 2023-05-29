bfc05a855fd5ca98f62418918e1501d9249647c1
SergioD <sergio_david2109@hotmail.com>
2023-05-28 21:25:09 -0600

    fix: adjust the module name for some imports

--- routes/comment.py
+++ routes/comment.py
@@ -1,17 +1,17 @@
-from fastapi import apirouter, depends, status
-from typing import list
-from schemas.comment import comment
+from fastapi import APIRouter, Depends, status
+from typing import List
+from schemas.comment import Comment
 from db_config.database import get_db
-from sqlalchemy.orm import session
-from services.comment import commentService
+from sqlalchemy.orm import Session
+from services.comment import CommentService
 
 
-comments_router = apirouter(prefix='/comments', comments=['comments'])
+comments_router = APIRouter(prefix='/comments', tags=['comments'])
 
 
 @comments_router.get('/',
-                     response_model=list[comment],
-                     status_code=status.http_200_ok)
-async def get_comments(db: session = depends(get_db)):
-    comments = await commentService().get_comments(db)
+                     response_model=List[Comment],
+                     status_code=status.HTTP_200_OK)
+async def get_comments(db: Session = Depends(get_db)):
+    comments = await CommentService().get_comments(db)
     return comments
