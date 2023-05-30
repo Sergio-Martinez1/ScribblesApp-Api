5c6c5c5965e9eb1d56769f3aca61e2d37274c912
mutska <mutska@ciencias.unam.mx>
2023-05-29 19:26:38 -0600

    feat(router): add operations for comments router
        - add post comment operation
        - add update comment operation
        - add delete comment operation

--- routes/comment.py
+++ routes/comment.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 from typing import List
 from schemas.comment import Comment
 from db_config.database import get_db
@@ -15,3 +16,30 @@ comments_router = APIRouter(prefix='/comments', tags=['comments'])
 async def get_comments(db: Session = Depends(get_db)):
     comments = await CommentService().get_comments(db)
     return comments
+
+
+@comments_router.post('/',
+                      response_model=dict,
+                      status_code=status.HTTP_201_CREATED)
+async def create_comment(comment: Comment,
+                         db: Session = Depends(get_db)):
+    await CommentService().create_comment(comment, db)
+    return JSONResponse(content={"message": "Succesful comment created."})
+
+
+@comments_router.put('/{id}',
+                     response_model=dict,
+                     status_code=status.HTTP_200_OK)
+async def update_comment(id: int, comment: Comment,
+                         db: Session = Depends(get_db)):
+    await CommentService().update_comment(id, comment, db)
+    return JSONResponse(content={"message": "Succesful comment update."})
+
+
+@comments_router.delete('/{id}',
+                        response_model=dict,
+                        status_code=status.HTTP_200_OK)
+async def delete_comment(id: int,
+                         db: Session = Depends(get_db)):
+    await CommentService().delete_comment(id, db)
+    return JSONResponse(content={"message": "Succesful comment deleted."})
