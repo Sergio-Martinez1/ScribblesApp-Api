2e05c0c39bcdc901541126207b8b9f854022f2dd
mutska <mutska@ciencias.unam.mx>
2023-05-29 19:13:00 -0600

    feat(router): add operations for reaction router
    - add post reaction operation
    - add delete reaction operation

--- routes/reaction.py
+++ routes/reaction.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 from typing import List
 from schemas.reaction import Reaction
 from db_config.database import get_db
@@ -14,3 +15,21 @@ reactions_router = APIRouter(prefix='/reactions', tags=['reactions'])
 async def get_reactions(db: Session = Depends(get_db)):
     reactions = await ReactionService().get_reactions(db)
     return reactions
+
+
+@reactions_router.post('/',
+                       response_model=dict,
+                       status_code=status.HTTP_201_CREATED)
+async def create_reaction(reaction: Reaction,
+                          db: Session = Depends(get_db)):
+    await ReactionService().create_reaction(reaction, db)
+    return JSONResponse(content={"message": "Succesful reaction created."})
+
+
+@reactions_router.delete('/{id}',
+                         response_model=dict,
+                         status_code=status.HTTP_200_OK)
+async def delete_reaction(id: int,
+                          db: Session = Depends(get_db)):
+    await ReactionService().delete_reaction(id, db)
+    return JSONResponse(content={"message": "Succesful reaction created."})
