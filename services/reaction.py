23289198c3d18482d2c7bc9305c853eced08ce16
mutska <mutska@ciencias.unam.mx>
2023-05-29 19:11:30 -0600

    feat(service): add services for reaction
    - add create reaction service
    - add delete reaction service

--- services/reaction.py
+++ services/reaction.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from models.models import Reaction as ReactionModel
+from schemas.reaction import Reaction
 
 
 class ReactionService():
@@ -8,3 +9,17 @@ class ReactionService():
 
     async def get_reactions(self, db: Session):
         return db.query(ReactionModel).all()
+
+    async def create_reaction(self, reaction: Reaction, db: Session):
+        new_reaction = ReactionModel(**reaction.dict())
+        db.add(new_reaction)
+        db.commit()
+        return
+
+    async def delete_reaction(self, id: int, db: Session):
+        result = db.query(ReactionModel).filter(ReactionModel.id == id).first()
+        if not result:
+            return
+        db.delete(result)
+        db.commit()
+        return
