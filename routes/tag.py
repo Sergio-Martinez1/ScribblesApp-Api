708a94641f9bc9586eb12f37b19ad4e3287e00d3
mutska <mutska@ciencias.unam.mx>
2023-05-29 19:32:25 -0600

    feat(router): add operations for tags router
      - add post tag operation
      - add delete tag operation

--- routes/tag.py
+++ routes/tag.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 from typing import List
 from schemas.tag import Tag
 from db_config.database import get_db
@@ -15,3 +16,21 @@ tags_router = APIRouter(prefix='/tags', tags=['tags'])
 async def get_tags(db: Session = Depends(get_db)):
     tags = await TagService().get_tags(db)
     return tags
+
+
+@tags_router.post('/',
+                  response_model=dict,
+                  status_code=status.HTTP_201_CREATED)
+async def create_tag(tag: Tag,
+                     db: Session = Depends(get_db)):
+    await TagService().create_tag(tag, db)
+    return JSONResponse(content={"message": "Succesful tag created."})
+
+
+@tags_router.delete('/{id}',
+                    response_model=dict,
+                    status_code=status.HTTP_200_OK)
+async def delete_tag(id: int,
+                     db: Session = Depends(get_db)):
+    await TagService().delete_tag(id, db)
+    return JSONResponse(content={"message": "Succesful tag deleted."})
