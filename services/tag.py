5e73fec2426d463fd7f9ade2301cf3f44b8ae9cd
mutska <mutska@ciencias.unam.mx>
2023-05-29 19:31:39 -0600

    feat(service): add services for tags
      - add create tag service
      - add delete tag service

--- services/tag.py
+++ services/tag.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from models.models import Tag as TagModel
+from schemas.tag import Tag
 
 
 class TagService():
@@ -8,3 +9,17 @@ class TagService():
 
     async def get_tags(self, db: Session):
         return db.query(TagModel).all()
+
+    async def create_tag(self, tag: Tag, db: Session):
+        new_tag = TagModel(**tag.dict())
+        db.add(new_tag)
+        db.commit()
+        return
+
+    async def delete_tag(self, id: int, db: Session):
+        result = db.query(TagModel).filter(TagModel.id == id).first()
+        if not result:
+            return
+        db.delete(result)
+        db.commit()
+        return
