7d4ec47bb43f73dc9f13e321388f4be2980ed813
mutska <mutska@ciencias.unam.mx>
2023-05-29 19:25:42 -0600

    feat(service): add services for comments
        - add create comment service
        - add update comment service
        - add delete comment service

--- services/comment.py
+++ services/comment.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from models.models import Comment as CommentModel
+from schemas.comment import Comment
 
 
 class CommentService():
@@ -8,3 +9,25 @@ class CommentService():
 
     async def get_comments(self, db: Session):
         return db.query(CommentModel).all()
+
+    async def create_comment(self, comment: Comment, db: Session):
+        new_comment = CommentModel(**comment.dict())
+        db.add(new_comment)
+        db.commit()
+        return
+
+    async def update_comment(self, id: int,  comment: Comment, db: Session):
+        comment = db.query(CommentModel).filter(CommentModel.id == id).first()
+        comment.update({
+            CommentModel.content: Comment.content
+        })
+        db.commit()
+        return
+
+    async def delete_comment(self, id: int, db: Session):
+        result = db.query(CommentModel).filter(CommentModel.id == id).first()
+        if not result:
+            return
+        db.delete(result)
+        db.commit()
+        return
