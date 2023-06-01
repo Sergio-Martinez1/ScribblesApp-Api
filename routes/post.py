9f95275aaa472a277d298837c6ba278e9a549d97
SergioD <sergio_david2109@hotmail.com>
2023-05-31 20:03:16 -0600

    refactor(router): add CRUD for posts

--- routes/post.py
+++ routes/post.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, status, Depends
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from services.post import PostService
 from db_config.database import get_db
 from typing import List
-from schemas.post import Post
+from schemas.post import Post, DbPost
 
 posts_router = APIRouter(prefix='/posts', tags=['posts'])
 
@@ -11,6 +12,30 @@ posts_router = APIRouter(prefix='/posts', tags=['posts'])
 @posts_router.get('/',
                   response_model=List[Post],
                   status_code=status.HTTP_200_OK)
-async def get_posts(db: Session = Depends(get_db)):
+async def get_posts(db: Session = Depends(get_db)) -> List[Post]:
     posts = await PostService().get_posts(db)
     return posts
+
+
+@posts_router.post('/',
+                   response_model=dict,
+                   status_code=status.HTTP_201_CREATED)
+async def create_post(post: DbPost, db: Session = Depends(get_db)) -> dict:
+    await PostService().create_post(db, post)
+    return JSONResponse(content={'message': 'Post sucessfully created'},
+                        status_code=status.HTTP_201_CREATED)
+
+
+@posts_router.put('/{id}', response_model=dict, status_code=status.HTTP_200_OK)
+async def update_post(id: int, post: Post,
+                      db: Session = Depends(get_db)) -> dict:
+    await PostService().update_post(db, post, id)
+    return JSONResponse(content={'message': 'Post sucessfully updated'},
+                        status_code=status.HTTP_200_OK)
+
+
+@posts_router.delete('/{id}', status_code=status.HTTP_200_OK)
+async def delete_post(id: int, db: Session = Depends(get_db)) -> dict:
+    await PostService().delete_post(db, id)
+    return JSONResponse(content={'message': 'Post sucessfully deleted'},
+                        status_code=status.HTTP_200_OK)
