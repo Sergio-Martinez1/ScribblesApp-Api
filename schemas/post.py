bfdb06caa27f423bad997199ef26aa91ef91d3db
SergioD <sergio_david2109@hotmail.com>
2023-05-31 20:04:02 -0600

    feat(schemas): add a db_schema for posts

--- schemas/post.py
+++ schemas/post.py
@@ -20,3 +20,16 @@ class Post(BaseModel):
 
     class Config():
         orm_mode = True
+
+
+class DbPost(BaseModel):
+    title: str = Field(min_length=1, max_length=1000)
+    thumbnail: str = Field(None)
+    content: str = Field(min_length=1, max_length=1000)
+    publication_date: date = Field(...,
+                                   example="2023-05-28",
+                                   description="My date field")
+    user_id: int
+
+    class Config():
+        orm_mode = True
