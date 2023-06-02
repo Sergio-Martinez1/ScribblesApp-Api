ddcfd57c6cf52be8f08921878486e33e04e47567
SergioD <sergio_david2109@hotmail.com>
2023-06-01 19:06:46 -0600

    feat(CORS): add a cors middleware

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from routes.comment import comments_router
 from routes.tag import tags_router
 from routes.post import posts_router
 from db_config.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.title = "Scribbles API"
 app.version = "0.0.1"
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 app.include_router(users_route)
 app.include_router(reactions_router)
 app.include_router(tags_router)
