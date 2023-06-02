61ffa022ff86000006c250974ea70654ea3b407f
SergioD <sergio_david2109@hotmail.com>
2023-06-01 19:02:39 -0600

    feat(auth): add authentication layer for users routes and services

--- routes/user.py
+++ routes/user.py
@@ -2,9 +2,11 @@ from typing import List
 from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
 from schemas.user import User
+from schemas.user import TokenResponse
 from sqlalchemy.orm import Session
 from db_config.database import get_db
 from services.user import UserService
+from fastapi.security import OAuth2PasswordRequestForm
 
 users_route = APIRouter(prefix='/users', tags=['user'])
 
@@ -17,7 +19,19 @@ async def get_users(db: Session = Depends(get_db)) -> List[User]:
     return users
 
 
-@users_route.post('/', status_code=status.HTTP_201_CREATED)
-async def create_user(user: User, db: Session = Depends(get_db)):
-    user = await UserService().create_user(user, db)
-    return JSONResponse(content={"message": "Successful user update."})
+@users_route.post('/signup',
+                  status_code=status.HTTP_201_CREATED,
+                  response_model=dict)
+async def sign_user_up(user: User, db: Session = Depends(get_db)) -> dict:
+
+    await UserService().create_user(user, db)
+    return JSONResponse(content={"message": "User created succesfully."},
+                        status_code=status.HTTP_201_CREATED)
+
+
+@users_route.post('/signin', response_model=TokenResponse)
+async def sign_user_in(request: OAuth2PasswordRequestForm = Depends(),
+                       db: Session = Depends(get_db)) -> TokenResponse:
+
+    return await UserService().login_user(request.username, request.password,
+                                          db)
--- services/user.py
+++ services/user.py
@@ -1,6 +1,11 @@
 from models.models import User as UserModel
 from schemas.user import User
 from sqlalchemy.orm import Session
+from auth.hash_password import HashPassword
+from auth.jwt_handler import create_access_token
+from fastapi import HTTPException, status
+
+hash_password = HashPassword()
 
 
 class UserService():
@@ -15,6 +20,29 @@ class UserService():
         return db.query(UserModel).filter(UserModel.id == id).first()
 
     async def create_user(self, user: User, db: Session):
+        hashed_password = hash_password.create_hash(user.password)
+        user.password = hashed_password
         new_user = UserModel(**user.dict())
         db.add(new_user)
         db.commit()
+        return
+
+    async def login_user(self, username: str, password: str, db: Session):
+        dbUser = db.query(UserModel).filter(
+            UserModel.username == username).first()
+
+        if not dbUser:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="User does not exist")
+
+        if hash_password.verify_hash(password, dbUser.password):
+            access_token = create_access_token(dbUser.username)
+            return {
+                "access_token": access_token,
+                "token_type": "Bearer",
+                "user_id": dbUser.id,
+                "username": dbUser.username
+            }
+        else:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                detail="Incorrect password")
