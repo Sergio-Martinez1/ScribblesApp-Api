853e01875f2bea509ca368a6f11cb10389e81791
SergioD <sergio_david2109@hotmail.com>
2023-06-01 19:01:10 -0600

    feat(auth): add authentication layer for posts routes and services

--- routes/post.py
+++ routes/post.py
@@ -4,15 +4,17 @@ from sqlalchemy.orm import Session
 from services.post import PostService
 from db_config.database import get_db
 from typing import List
-from schemas.post import Post, DbPost
+from schemas.post import PostIn, PostOut
+from auth.authenticate import authenticate
 
 posts_router = APIRouter(prefix='/posts', tags=['posts'])
 
 
 @posts_router.get('/',
-                  response_model=List[Post],
+                  response_model=List[PostOut],
                   status_code=status.HTTP_200_OK)
-async def get_posts(db: Session = Depends(get_db)) -> List[Post]:
+async def get_posts(db: Session = Depends(get_db)) -> List[PostOut]:
+
     posts = await PostService().get_posts(db)
     return posts
 
@@ -20,22 +22,35 @@ async def get_posts(db: Session = Depends(get_db)) -> List[Post]:
 @posts_router.post('/',
                    response_model=dict,
                    status_code=status.HTTP_201_CREATED)
-async def create_post(post: DbPost, db: Session = Depends(get_db)) -> dict:
-    await PostService().create_post(db, post)
+async def create_post(
+    post: PostIn,
+    db: Session = Depends(get_db),
+    user: str = Depends(authenticate)
+) -> dict:
+
+    await PostService().create_post(db, post, user)
     return JSONResponse(content={'message': 'Post sucessfully created'},
                         status_code=status.HTTP_201_CREATED)
 
 
 @posts_router.put('/{id}', response_model=dict, status_code=status.HTTP_200_OK)
-async def update_post(id: int, post: Post,
-                      db: Session = Depends(get_db)) -> dict:
-    await PostService().update_post(db, post, id)
+async def update_post(
+    id: int,
+    post: PostIn,
+    db: Session = Depends(get_db),
+    user: str = Depends(authenticate)
+) -> dict:
+
+    await PostService().update_post(db, post, id, user)
     return JSONResponse(content={'message': 'Post sucessfully updated'},
                         status_code=status.HTTP_200_OK)
 
 
 @posts_router.delete('/{id}', status_code=status.HTTP_200_OK)
-async def delete_post(id: int, db: Session = Depends(get_db)) -> dict:
-    await PostService().delete_post(db, id)
+async def delete_post(
+    id: int, db: Session = Depends(get_db), user: str = Depends(authenticate)
+) -> dict:
+
+    await PostService().delete_post(db, id, user)
     return JSONResponse(content={'message': 'Post sucessfully deleted'},
                         status_code=status.HTTP_200_OK)
--- services/post.py
+++ services/post.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
 from models.models import Post as PostModel
-from schemas.post import Post, DbPost
+from models.models import User as UserModel
+from schemas.post import PostIn
+from fastapi import HTTPException, status
+from datetime import date
 
 
 class PostService():
@@ -11,19 +14,34 @@ class PostService():
     async def get_posts(self, db: Session):
         return db.query(PostModel).all()
 
-    async def create_post(self, db: Session, post: DbPost):
+    async def create_post(self, db: Session, post: PostIn, username: str):
+        # Request the user id in the database
+        creator = db.query(UserModel).filter(
+            UserModel.username == username).first()
+        # Validate that the user exist
+        if not creator:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="User not found, sign in again")
+
+        # Filling the fields for the database post model
         new_post = PostModel(title=post.title,
                              thumbnail=post.thumbnail,
                              content=post.content,
-                             publication_date=post.publication_date,
-                             user_id=post.user_id)
+                             publication_date=date.today(),
+                             user_id=creator.id)
         db.add(new_post)
         db.commit()
         db.refresh(new_post)
         return
 
-    async def update_post(self, db: Session, new_post: Post, id: int):
+    async def update_post(self, db: Session, new_post: PostIn, id: int,
+                          username: str):
         post = db.query(PostModel).filter(PostModel.id == id).first()
+        creator = db.query(UserModel).filter(
+            UserModel.id == post.user_id).first()
+        if username != creator.username:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="Operation not allowed")
         post.title = new_post.title
         post.thumbnail = new_post.thumbnail
         post.content = new_post.content
@@ -31,7 +49,13 @@ class PostService():
         db.commit()
         return
 
-    async def delete_post(self, db: Session, id: int):
+    async def delete_post(self, db: Session, id: int, username: str):
+        post = db.query(PostModel).filter(PostModel.id == id).first()
+        creator = db.query(UserModel).filter(
+            UserModel.id == post.user_id).first()
+        if username != creator.username:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="Operation not allowed")
         post = db.query(PostModel).filter(PostModel.id == id).first()
         if not post:
             return
