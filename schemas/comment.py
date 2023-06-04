30ad28850f3c08e2df964c6fa0114c0002e67b39
SergioD <sergio_david2109@hotmail.com>
2023-06-04 14:57:23 -0600

    refactor(schemas): add a new schema and rename the existing one
    Add the CommentIn and CommentOut schemas, one for users requests and the
    other for server responses

--- schemas/comment.py
+++ schemas/comment.py
@@ -1,15 +1,43 @@
 from pydantic import BaseModel, Field
 
 
-class Comment(BaseModel):
-    id: int
+class CommentIn(BaseModel):
     content: str = Field(default="", max_length=1000)
+    post_id: int
+
+    class Config():
+        schema_extra = {
+            "example": {
+                "content": "Some content for a comment",
+                "post_id": 1
+            }
+        }
+        orm_mode = True
+
+
+class CommentToEdit(BaseModel):
+    content: str = Field(default="", max_length=1000)
+
+    class Config():
+        schema_extra = {
+            "example": {
+                "content": "Some content for a comment",
+            }
+        }
+        orm_mode = True
+
+
+class CommentOut(BaseModel):
+    content: str = Field(default="", max_length=1000)
+    post_id: int
     user_id: int
 
     class Config():
         schema_extra = {
             "example": {
                 "content": "Some content for a comment",
+                "post_id": 1,
+                "user_id": 1
             }
         }
         orm_mode = True
