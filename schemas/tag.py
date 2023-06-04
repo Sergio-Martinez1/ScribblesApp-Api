81ea4c1a430c84efcc92a8e9f89783d878d02810
SergioD <sergio_david2109@hotmail.com>
2023-06-04 14:21:43 -0600

    fix(schemas): add post_id field to bind tags and posts

--- schemas/tag.py
+++ schemas/tag.py
@@ -2,13 +2,14 @@ from pydantic import BaseModel, Field
 
 
 class Tag(BaseModel):
-    id: int
     content: str = Field(default=None)
+    post_id: int
 
     class Config():
         schema_extra = {
             "example": {
                 "content": "Movies",
+                "post_id": 1
             }
         }
         orm_mode = True
