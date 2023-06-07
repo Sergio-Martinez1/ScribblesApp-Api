49ea06b6fe066ba5caa0797e411d141f08d949f1
SergioD <sergio_david2109@hotmail.com>
2023-06-07 17:52:07 -0600

    test(comment_router): add comment router tests

---
+++ test/test_comment.py
@@ -0,0 +1,117 @@
+import httpx
+import pytest
+from datetime import date
+from auth.jwt_handler import create_access_token
+from auth.hash_password import HashPassword
+from db_config.database import SessionLocal
+from models.models import User
+from models.models import Post
+from models.models import Comment
+
+db = SessionLocal()
+
+
+@pytest.fixture(scope="module")
+async def mock_user() -> User:
+    test_password = "123456"
+    hashed_password = HashPassword().create_hash(test_password)
+    userModel = User(
+        username="sergio",
+        email="sergio@mail.com",
+        password=hashed_password,
+        image="http://image.com",
+    )
+    db.add(userModel)
+    db.commit()
+    db.refresh(userModel)
+    stored_user = db.query(User).filter(
+        User.username == userModel.username).first()
+    yield stored_user
+
+
+@pytest.fixture(scope="module")
+async def mock_access_token(mock_user: User) -> str:
+    return create_access_token(mock_user.username)
+
+
+@pytest.fixture(scope="module")
+async def mock_post(mock_user: User) -> Post:
+    new_post = Post(title="Title for a test post",
+                    thumbnail="http://image.com",
+                    content="Some content for a test post",
+                    publication_date=date.today(),
+                    user_id=mock_user.id)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    stored_post = db.query(Post).filter(Post.user_id == mock_user.id).first()
+    yield stored_post
+
+
+@pytest.fixture(scope="module")
+async def mock_comment(mock_user: User, mock_post: Post) -> Comment:
+    new_comment = Comment(post_id=mock_post.id,
+                          user_id=mock_user.id,
+                          content="Some content for a test post")
+    db.add(new_comment)
+    db.commit()
+    db.refresh(new_comment)
+    stored_comment = db.query(Comment).filter(
+        Comment.user_id == mock_user.id).first()
+    yield stored_comment
+
+
+@pytest.mark.asyncio
+async def test_create_comment(default_client: httpx.AsyncClient,
+                              mock_access_token: str, mock_post: Post):
+    payload = {
+        "content": "Some content for a comment",
+        "post_id": mock_post.id
+    }
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {mock_access_token}"
+    }
+    test_response = {"message": "Succesful comment created."}
+    response = await default_client.post("/comments/",
+                                         json=payload,
+                                         headers=headers)
+
+    assert response.status_code == 201
+    assert response.json() == test_response
+
+
+@pytest.mark.asyncio
+async def test_update_comment(default_client: httpx.AsyncClient,
+                              mock_access_token: str, mock_comment: Comment):
+    payload = {"content": "Some content for a comment"}
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {mock_access_token}"
+    }
+    test_response = {"message": "Succesful comment update."}
+
+    response = await default_client.put(f"/comments/{mock_comment.id}",
+                                        json=payload,
+                                        headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == test_response
+
+
+@pytest.mark.asyncio
+async def test_delete_comment(default_client: httpx.AsyncClient,
+                              mock_access_token: str, mock_comment: Comment):
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {mock_access_token}"
+    }
+    test_response = {"message": "Succesful comment deleted."}
+    response = await default_client.delete(f"/comments/{mock_comment.id}",
+                                           headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == test_response
