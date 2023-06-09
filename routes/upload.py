ded11cd87c111dcc6276dfbc402488afe292463c
SergioD <sergio_david2109@hotmail.com>
2023-06-08 21:16:43 -0600

    refactor(router): change the prefix and how to persisting files

--- routes/upload.py
+++ routes/upload.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter, UploadFile, File, Depends
+import shutil
+from fastapi import APIRouter, UploadFile, File, Depends, Request
 from auth.authenticate import authenticate
 
-upload_router = APIRouter(prefix='/upload', tags=['Files'])
+upload_router = APIRouter(prefix="/files", tags=["Files"])
 
 
-@upload_router.post('/')
-async def upload_file(file: UploadFile = File(...),
+@upload_router.post("/")
+async def upload_file(request: Request,
+                      file: UploadFile = File(...),
                       user: str = Depends(authenticate)):
     path = f"files/{file.filename}"
-    return {"path": path, "filename": file.filename}
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    url = f"{request.url}" + f"{file.filename}"
+    return {"url": url, "type": file.content_type}
