a9b19e23086ef62073134253f30d0f71b66604e3
SergioD <sergio_david2109@hotmail.com>
2023-06-08 21:18:57 -0600

    feat(files): add a static files directory

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from routes.post import posts_router
 from routes.upload import upload_router
 from db_config.database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -23,5 +24,6 @@ app.include_router(tags_router)
 app.include_router(comments_router)
 app.include_router(posts_router)
 app.include_router(upload_router)
+app.mount('/files', StaticFiles(directory='files'), name='files')
 
 Base.metadata.create_all(bind=engine)
