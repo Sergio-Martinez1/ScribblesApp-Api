6045aa08000d31bb4465b66761d7e5007269f0e8
SergioD <sergio_david2109@hotmail.com>
2023-06-26 22:34:17 -0600

    refactor(schemas): add creation date and user field
    Use a plain user for displaying more details about the comment

--- schemas/comment.py
+++ schemas/comment.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, Field
+from .user import PlainUser
+from datetime import date
 
 
 class CommentIn(BaseModel):
@@ -29,8 +31,12 @@ class CommentToEdit(BaseModel):
 
 class CommentOut(BaseModel):
     content: str = Field(default="", max_length=1000)
+    creation_date: date = Field(...,
+                                example="2023-05-28",
+                                description="My date field")
     post_id: int
     user_id: int
+    user: PlainUser
 
     class Config():
         schema_extra = {
