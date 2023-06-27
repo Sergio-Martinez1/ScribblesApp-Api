b812bb4686ef8f4ee1b0a7bcdedb2bd68c3bf116
SergioD <sergio_david2109@hotmail.com>
2023-06-26 22:31:45 -0600

    refactor(models): add more fields in user and comment
    Add more details necessary in the frontend

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ARRAY
 from db_config.database import Base
 from sqlalchemy.schema import ForeignKey
 from sqlalchemy.orm import relationship
@@ -11,7 +11,18 @@ class User(Base):
     username = Column(String)
     password = Column(String)
     email = Column(String)
-    image = Column(String, nullable=True)
+    creation_date = Column(Date)
+
+    profile_photo = Column(String, nullable=True)
+    cover_photo = Column(String, nullable=True)
+    description = Column(String, nullable=True)
+    personal_url = Column(String, nullable=True)
+    location = Column(String, nullable=True)
+    birthday = Column(Date, nullable=True)
+    prohibited_posts = Column(ARRAY(Integer), nullable=True)
+    # A revision
+    posts = relationship('Post', back_populates='user')
+    comments = relationship('Comment', back_populates='user')
 
 
 class Tag(Base):
@@ -41,6 +52,7 @@ class Post(Base):
     content = Column(String)
     publication_date = Column(Date)
     user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='posts')
     tags = relationship('Tag', back_populates='post')
     reactions = relationship('Reaction', back_populates='post')
     comments = relationship('Comment', back_populates='post')
@@ -50,7 +62,9 @@ class Comment(Base):
     __tablename__ = "comments"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    post_id = Column(Integer, ForeignKey('posts.id'))
     user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
     content = Column(String)
+    creation_date = Column(Date)
+    user = relationship('User', back_populates='comments')
     post = relationship('Post', back_populates='comments')
