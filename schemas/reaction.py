2455274912c4deefb5adc576e11dbf4be14dc379
SergioD <sergio_david2109@hotmail.com>
2023-06-30 22:29:33 -0600

    feat(schemas): add reaction in and out schemas

--- schemas/reaction.py
+++ schemas/reaction.py
@@ -1,13 +1,27 @@
 from pydantic import BaseModel
 
 
-class Reaction(BaseModel):
+class ReactionIn(BaseModel):
+    post_id: int
+
+    class Config():
+        schema_extra = {
+            "example": {
+                "post_id": 1
+            }
+        }
+        orm_mode = True
+
+
+class ReactionOut(BaseModel):
+    id: int
     user_id: int
     post_id: int
 
     class Config():
         schema_extra = {
             "example": {
+                "id": 1,
                 "user_id": 1,
                 "post_id": 1
             }
