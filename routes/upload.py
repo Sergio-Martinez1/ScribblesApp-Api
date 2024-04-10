7829fe71b9a4b4a37f207be02551e731e59e09ca
SergioD <sergio_david2109@hotmail.com>
2024-04-09 19:07:21 -0600

    refactor(routes): add optimization for images

--- routes/upload.py
+++ routes/upload.py
@@ -1,6 +1,9 @@
-import shutil
 from fastapi import APIRouter, UploadFile, File, Depends, Request
 from auth.authenticate import authenticate
+from PIL import Image
+import io
+import uuid
+import os
 
 upload_router = APIRouter(prefix="/files", tags=["Files"])
 
@@ -9,8 +12,42 @@ upload_router = APIRouter(prefix="/files", tags=["Files"])
 async def upload_file(request: Request,
                       file: UploadFile = File(...),
                       user: str = Depends(authenticate)):
-    path = f"files/{file.filename}"
-    with open(path, "w+b") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    url = f"{request.url}" + f"{file.filename}"
+
+    unique_id = uuid.uuid4()
+    filename = f"image_{unique_id}.webp"
+
+    content = await file.read()
+    image = Image.open(io.BytesIO(content))
+    image = image.convert('RGB')
+
+    max_width = 1920
+    max_height = 1080
+
+    if image.width > max_width or image.height > max_height:
+        width_ratio = max_width / image.width
+        height_ratio = max_height / image.height
+        resize_ratio = min(width_ratio, height_ratio)
+
+        new_width = int(image.width * resize_ratio)
+        new_height = int(image.height * resize_ratio)
+
+        image = image.resize((new_width, new_height))
+
+    with io.BytesIO() as output:
+        image.save(output, format='WebP', quality=60)
+        output.seek(0)
+        optimized_image_bytes = output.read()
+
+    directory = "files/"
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    print(optimized_image_bytes)
+
+    file_path = os.path.join(directory, filename)
+    with open(file_path, "wb") as f:
+        f.write(optimized_image_bytes)
+
+    url = f"{request.url}" + f"{filename}"
+
     return {"url": url, "type": file.content_type}
