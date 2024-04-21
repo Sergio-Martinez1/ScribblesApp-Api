b01d313801b5cd9f7c48bbadb875270d39a08ed5
SergioD <sergio_david2109@hotmail.com>
2024-04-21 08:21:21 -0600

    refactor(tests): update configuration

--- test/conftest.py
+++ test/conftest.py
@@ -8,23 +8,14 @@ from models.models import Post
 from models.models import Tag
 from models.models import Reaction
 from models.models import Comment
+import pytest_asyncio
 from db_config.database import SessionLocal
 
 
-@pytest.fixture(scope='session')
-def event_loop():
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 async def default_client():
     async with httpx.AsyncClient(app=app, base_url="http://app") as client:
         yield client
-
-
-# Clean up resources
     db = SessionLocal()
     db.query(Comment).delete()
     db.query(Reaction).delete()
