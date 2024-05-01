0e4ac921e867194ac52ec8dd43d5ab6069d73b95
SergioD <sergio_david2109@hotmail.com>
2024-05-01 13:53:43 -0600

    chore(db_config): adjust env file name

--- db_config/connection.py
+++ db_config/connection.py
@@ -9,11 +9,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str = None
     ACCESS_TOKEN_EXPIRE_SECONDS: int = None
 
-    def get_db_url(self, test=False):
-        if test:
-            return self.TEST_DATABASE_URL
-        else:
-            return self.DATABASE_URL
+    def get_db_url(self):
+        return self.DATABASE_URL
 
     class Config:
         env_file = ".env"
