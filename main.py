f18cb35006008b604cac701e3be11bd60aecaea3
SergioD <sergio_david2109@hotmail.com>
2024-05-01 18:11:03 -0600

    refactor(router): update files route name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from routes.reaction import reactions_router
 from routes.comment import comments_router
 from routes.tag import tags_router
 from routes.post import posts_router
-from routes.upload import upload_router
+from routes.files import files_router
 from db_config.database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -24,7 +24,7 @@ app.include_router(reactions_router)
 app.include_router(tags_router)
 app.include_router(comments_router)
 app.include_router(posts_router)
-app.include_router(upload_router)
+app.include_router(files_router)
 app.mount('/files', StaticFiles(directory='files'), name='files')
 
 Base.metadata.create_all(bind=engine)
