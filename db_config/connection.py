ca69be29e0cc3a50f7050968f3fce9ec59bdd9e6
SergioD <sergio_david2109@hotmail.com>
2024-05-01 18:05:47 -0600

    feat(db_config): add cloud storage env variables

--- db_config/connection.py
+++ db_config/connection.py
@@ -8,6 +8,8 @@ class Settings(BaseSettings):
     TEST_DATABASE_URL: Optional[str] = None
     SECRET_KEY: str = None
     ACCESS_TOKEN_EXPIRE_SECONDS: int = None
+    BUCKET_URL: str = None
+    BUCKET_NAME: str = None
 
     def get_db_url(self):
         return self.DATABASE_URL
