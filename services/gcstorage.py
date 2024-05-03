233db6547d4abd9076c018dcea70e9c897ef61d8
SergioD <sergio_david2109@hotmail.com>
2024-05-03 12:03:30 -0600

    refactor(service): change env variables src

--- services/gcstorage.py
+++ services/gcstorage.py
@@ -1,4 +1,5 @@
 from google.cloud import storage
+from db_config.connection import settings
 
 
 class GCStorageService():
@@ -9,7 +10,7 @@ class GCStorageService():
     async def upload_file_to_bucket(self, filename: str, file,
                                     bucket_name: str):
         storage_client = storage.Client.from_service_account_json(
-            "ServiceKey_GoogleCloud.json")
+            settings.GOOGLE_APPLICATION_CREDENTIALS)
 
         try:
             bucket = storage_client.get_bucket(bucket_name)
@@ -22,7 +23,7 @@ class GCStorageService():
 
     async def delete_file_from_bucket(self, filename: str, bucket_name: str):
         storage_client = storage.Client.from_service_account_json(
-            "ServiceKey_GoogleCloud.json")
+            settings.GOOGLE_APPLICATION_CREDENTIALS)
         try:
             bucket = storage_client.get_bucket(bucket_name)
             blob = bucket.blob(filename)
