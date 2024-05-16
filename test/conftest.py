9af4d515d3d35f4f098a39bca40a20bb5c25a0b7
SergioD <sergio_david2109@hotmail.com>
2024-05-16 14:29:20 -0600

    fix(test): adjust event loop scope

--- test/conftest.py
+++ test/conftest.py
@@ -12,6 +12,13 @@ import pytest_asyncio
 from db_config.database import SessionLocal
 
 
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
 @pytest_asyncio.fixture(scope="session")
 async def default_client():
     async with httpx.AsyncClient(app=app, base_url="http://app") as client:
